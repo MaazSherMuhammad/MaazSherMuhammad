435f8c42fa879573e93ac1e417476cc5483706b5
MaazSherMuhammad <maazshermuhammadofficial@gmail.com>
2025-06-25 23:07:21 +0500

    updated

---
+++ Internship Task week 1/Addition.py
@@ -0,0 +1,5 @@
+# addition.py
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+result = num1 + num2
+print("The sum is:", result)
---
+++ Internship Task week 1/intro.py
@@ -0,0 +1,5 @@
+# intro.py
+print("Name: Maaz Sher Muhammad")
+print("Hobby: Cricket")
+print("Age: 20")
+print("Why I want to learn Python: Because it is beginner-friendly and useful for automation, data science, and web development.")
